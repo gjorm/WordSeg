384eed08a0905c0b3f37baf4fb4af9929f770354
Gjorm <jakemhaskins@gmail.com>
2020-07-18 13:58:08 -0600

    Add files via upload
    Added BiGram support

--- WordSeg.hpp
+++ WordSeg.hpp
@@ -43,7 +43,7 @@
 
 using namespace std;
 
-class WSUnigram
+class WSGram
 {
 private:
 	
@@ -52,12 +52,12 @@ public:
 	string gram;
 	double score;
 	
-	WSUnigram() {
+	WSGram() {
 		gram = "";
 		score = 0.0;
 	}
 	
-	WSUnigram(string str, double sc) {
+	WSGram(string str, double sc) {
 		gram = str;
 		score = sc;
 	}
@@ -81,7 +81,7 @@ public:
 	}
 };
 
-bool operator < (const WSUnigram &lhs, const WSUnigram &rhs) {
+bool operator < (const WSGram &lhs, const WSGram &rhs) {
 	return lhs.score < rhs.score;
 }
 
@@ -90,12 +90,14 @@ bool operator < (const WSUnigram &lhs, const WSUnigram &rhs) {
 class WordSeg
 {
 private:
-	unordered_map<string, WSUnigram> uniGrams;
-	unordered_map<string, vector<WSUnigram>> segMemo;//a hash map to memoize Segment()
+	unordered_map<string, WSGram> uniGrams;
+	unordered_map<string, WSGram> biGrams;
+	unordered_map<string, vector<WSGram>> segMemo;//a hash map to memoize Segment()
 	const double numCounts = 1024908267229;
 	double gMin = 1000000000.0, gMax = 0.0;
+	double bgMin = 1000000000.0, bgMax = 0.0;
 	const int maxSegLength = 24;
-	const int segMemoSize = 10000;//initial size for segMemo
+	const int segMemoSize = 10000;//arbitrary initial size for segMemo
 	int numIters = 0;
 	
 	//check for good ascii and numeric characters
@@ -105,6 +107,13 @@ private:
 				return true;
 		}
 
+		return false;
+	}
+	
+	bool IsDelimiter(char c) {
+		if(c == ' ' || c == '\n' || c == '\t')
+			return true;
+		
 		return false;
 	}
 
@@ -128,15 +137,15 @@ private:
 		return false;
 	}
 	
-	//Private inner method of Segment to manage segMemo
+	//Private inner recursive method of Segment
 	//Segment
-	vector<WSUnigram> pSegment(const string &in) {
-		WSUnigram wsLeft, wsRight;
-		vector<WSUnigram> cands;
-		vector<WSUnigram> result;
-		priority_queue<vector<WSUnigram>> candHeap;
-		WSUnigram def;
-		unordered_map<string, vector<WSUnigram>>::iterator mi;
+	vector<WSGram> pSegment(const string &in) {
+		WSGram wsLeft, wsRight;
+		vector<WSGram> cands;
+		vector<WSGram> result;
+		priority_queue<vector<WSGram>> candHeap;
+		WSGram def;
+		unordered_map<string, vector<WSGram>>::iterator mi;
 		
 		def.gram = "";
 		def.score = GetGramScore("");
@@ -160,13 +169,15 @@ private:
 					//recursively call pSegment on the right string
 					cands = pSegment(wsRight.gram);
 					
-					//score
+					//Score
+					//only need to score the left gram; the lower recursive depths will return this and higher depths will accept as the higher prob segment of right
 					wsLeft.score = GetGramScore(wsLeft.gram);
-					GetVecGramScore(cands);
 					
 					//store into result (result is being used as a worker, not a return val yet)
 					result.push_back(wsLeft);
 					result.insert(result.end(), cands.begin(), cands.end());
+					
+					//GetVecBiGramScore(result);
 					
 					//push onto the priority queue
 					candHeap.push(result);
@@ -176,12 +187,12 @@ private:
 					cands.clear();
 					
 					//this is commented out for now, but used for troubleshooting
-					//numIters++;
+					numIters++;
 				}
 			}
 			
 				
-			//grab the highest valued WSUniGram attempt from the priority_queue
+			//grab the highest valued WSGram attempt from the priority_queue
 			result = candHeap.top();
 			
 			//insert the vector into the segMemo hash table, as it was not found at the beginning of the method
@@ -194,34 +205,44 @@ private:
 
 	
 public:
-	WordSeg(const char* fileName) {
+	WordSeg(const char *uniGramFile, const char *biGramFile) {
 		ifstream inFile;
-		inFile.open(fileName);
+		unordered_map<string, WSGram>::iterator gi;
+		pair<unordered_map<string, WSGram>::iterator, bool> res;
+		
+		//File Input for Unigrams
+		inFile.open(uniGramFile);
 		if(inFile.is_open()) {
 			char c;
-			WSUnigram foo;
+			WSGram foo;
 			string get = "";
 			bool alt = true;
 
 			c = inFile.get();
-			while (inFile.good()) {
+			while (inFile.good() && !inFile.eof()) {
 
 				if (IsAlphabetic(c) || IsNumeric(c)) {
 					get += towupper(c);
 				}
 				else {
-					if (alt) {//first portion int he line is the gram
+					if (alt) {//first portion in the line is the gram
 						foo.SetGram(get);
 						alt = false;
 						get = "";
 					}
 					else {//second is the score.
-						foo.SetScore(stod(get) / numCounts); //make sure to divide by numCounts and that low prob grams are 1 / numCounts in other code
+						foo.SetScore(stod(get) / numCounts); //store raw counts
 						alt = true;
 						get = "";
 						
 						//make the insert after the score is found
-						uniGrams.insert(make_pair(foo.GetGram(), foo));
+						//attempt the insert
+						res = uniGrams.insert(make_pair(foo.GetGram(), foo));
+						//if unique, the WSGram will successfully insert into unordered_map, if not, then there are duplicates in the file so add the found count with the existing count
+						if (!res.second) {
+							gi = res.first;
+							gi->second.score += foo.GetScore();
+						}
 						
 						//update minima and maxima
 						if(gMin > foo.GetScore()) gMin = foo.GetScore();
@@ -233,26 +254,85 @@ public:
 			}
 		}
 		else {
-			cout << "WordSeg could not open the file for parsing: " << fileName << endl;
+			cout << "WordSeg could not open the file for parsing: " << uniGramFile << endl;
 			return;
 		}
+		inFile.close();
 		
+		
+		//File Input for Bi Grams
+		inFile.open(biGramFile);
+		if(inFile.is_open()) {
+			WSGram foo;
+			string get = "";
+			int alt = 0;
+			char c;
+
+			while (inFile.good()) {
+				c = inFile.get();
+				
+				if (!IsDelimiter(c) && !inFile.eof()) {
+					get += towupper(c);
+				}
+				else {
+					if (alt == 0) {//first two portions in the line is the gram
+						foo.SetGram(get);
+						alt++;
+						get = "";
+					}
+					else if(alt == 1) {
+						foo.SetGram(foo.GetGram() + " " + get);
+						alt++;
+						get = "";
+						
+					}
+					else if(alt == 2) {//last is the score.
+						foo.SetScore(stod(get) / numCounts); //store the raw counts
+						alt = 0;
+						get = "";
+						
+						//make the insert after the score is found
+						//attempt the insert
+						res = biGrams.insert(make_pair(foo.GetGram(), foo));
+						//if unique, the WSGram will successfully insert into unordered_map, if not, then there are duplicates in the file so add the found count with the existing count
+						if (!res.second) {
+							gi = res.first;
+							gi->second.score += foo.GetScore();
+						}
+						
+						//update minima and maxima
+						if(bgMin > foo.GetScore()) bgMin = foo.GetScore();
+						if(bgMax < foo.GetScore()) bgMax = foo.GetScore();
+					}
+				}
+			}
+		}
+		else {
+			cout << "WordSeg could not open the file for parsing: " << biGramFile << endl;
+			return;
+		}
+		inFile.close();
+		
+		//Set a large bucket size for the Memo Hash Table
 		segMemo.rehash(segMemoSize);
 
 	}
 	
 	~WordSeg() {
 		uniGrams.clear();
+		biGrams.clear();
 		segMemo.clear();
 	}
 	
 
-	//Public Accessible Segment function
-	vector<WSUnigram> Segment(const string &in) {
-		vector<WSUnigram> result = pSegment(in);
+	//Public Accessible Segment function for managing the segMemo hash table
+	vector<WSGram> Segment(const string &in) {
+		vector<WSGram> result = pSegment(in);
 
-		//may need to comment this out during a HillClimb() then call ClearSegMemo() after each restart
-		segMemo.clear();
+		//may need to comment this out during a HillClimb() then call ClearSegMemo() after a more ideal period as
+		//segMemo will maintain return values that benefit other runs of Segment, even with different input
+		//segMemo.clear();
+		numIters = 0;
 		
 		return result;
 	}
@@ -267,16 +347,14 @@ public:
 	
 	//calculate score given a single word gram
 	double GetGramScore(const string &in) {
-		unordered_map<string, WSUnigram>::iterator gi;
+		unordered_map<string, WSGram>::iterator gi;
 		double result = 0.0;
-		string test = "";
 		
 		if(in.size() <= 0 || in == "") {
-			result = log10(1.0 / numCounts);
+			result = log10(1.0);
 		}
 		else {
-			test = str_upper(in);
-			gi = uniGrams.find(test);
+			gi = uniGrams.find(in);
 			
 			if (gi != uniGrams.end()) {
 				result = log10(gi->second.GetScore());
@@ -289,16 +367,104 @@ public:
 		return result;
 	}
 	
-	void GetVecGramScore(vector<WSUnigram> &vG) {
-		
+	//calculate a vector of grams' scores
+	double GetVecGramScore(vector<WSGram> &vG) {
+		double total = 0.0;
 		for(int i = 0; i < (int)vG.size(); i++) {
 			vG[i].score = GetGramScore(vG[i].gram);
+			total += vG[i].score;
 		}
 		
-		return;
+		return total;
 	}
 	
-	string str_upper(const string &in) {
+	//retrieve a vector of grams as a string
+	string GetVecGram(const vector<WSGram> &vG) {
+		string result = "";
+		
+		for(int i = 0; i < (int)vG.size(); i++) {
+			result += vG[i].gram + " ";
+		}
+		
+		return result;
+	}
+	
+	//retrieve a vector of grams consolidated into a pair with the sum of its scores and a string of its concatenated grams
+	pair<double, string> GetVecPair(vector<WSGram> &vG) {
+		pair<double, string> result;
+		
+		for(int i = 0; i < (int)vG.size(); i++) {
+			result.first += vG[i].score;
+			result.second += vG[i].gram + " ";
+		}
+		
+		return result;
+	}
+	
+	//get the raw count (no log10 or division by numCounts) of a gram
+	double GetGramRawCount(const string &in) {
+		double result;
+		unordered_map<string, WSGram>::iterator gi;
+		
+		if(in.size() <= 0 || in == "") {
+			result = 0;
+		}
+		else {
+			gi = uniGrams.find(in);
+			
+			if (gi != uniGrams.end()) {
+				result = gi->second.GetScore();
+			}
+			else {
+				result = (gMin / 2) - (int)in.size();
+			}
+		}
+		
+		return result;
+	}
+	
+	//calculate score given a word-biGram
+	double GetBiGramScore(const string &left, const string &right) {
+		unordered_map<string, WSGram>::iterator gi;
+		double result = 0.0;
+		string test = left + " " + right;
+		
+		if(test.size() <= 0 || test == "") {
+			result = log10(1.0 / numCounts);
+		}
+		else {
+			gi = biGrams.find(test);
+			
+			if (gi != uniGrams.end()) {
+				result = log10(gi->second.GetScore());
+			}
+			else {
+				//on failure to find anything in the bigrams, fall back to unigrams for the right string
+				result += GetGramScore(right);
+			}
+		}
+
+		return result;
+	}
+	
+	//score a vector of grams using biGram data
+	double GetVecBiGramScore(vector<WSGram> &vec) {
+		double result;
+		
+		vec[0].score = GetGramScore(vec[0].gram);
+		
+		result = vec[0].score;
+		
+		for(int i = 1; i < (int)vec.size(); i++) {
+			vec[i].score = GetBiGramScore(vec[i - 1].gram, vec[i].gram);
+			result += vec[i].score;
+		}
+		
+		return result;
+	}
+	
+	//convert an entire string to upper case
+	string StringUpper(const string &in) {
 		string result;
 		
 		for(int i = 0; i < (int)in.size(); i++) {
@@ -308,6 +474,7 @@ public:
 		return result;
 	}
 	
+	//regular accessors
 	int GetNumGrams() {
 		return (int)uniGrams.size();
 	}
@@ -323,8 +490,8 @@ public:
 	}
 };
 
-
-bool operator < (const vector<WSUnigram> &lhs, const vector<WSUnigram> &rhs) {
+//operator overload for the priority queue of a vector of grams
+bool operator < (const vector<WSGram> &lhs, const vector<WSGram> &rhs) {
 	double lScore = 0;
 	double rScore = 0;
 	
